f78ef9af985c37921049d31a57675718fa3eda40
Jacob Parker <jacob@solidangle.ca>
2020-06-22 18:01:33 -0400

    ypo

--- lint.py
+++ lint.py
@@ -31,7 +31,7 @@ class Linter:
 
     # Per-job lints
     for jobId in workflow['jobs']:
-      self.checkForTimeout(workflowPath, jobId, workflow['jobs'][jobId])
+      self.checkForTimeout(self.workflowPath, jobId, workflow['jobs'][jobId])
 
     if addMarkers:
       print('::endgroup::')

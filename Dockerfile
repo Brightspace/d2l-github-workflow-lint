26d35dc43948facfc7ecc94addc58e2c653b0570
Jacob Parker <jacob@solidangle.ca>
2020-06-22 17:18:34 -0400

    Implement the action, with Python. (#1)

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM python:3.8.3-alpine3.11
+WORKDIR /app
+
+COPY requirements.txt ./
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY lint.py ./
+
+CMD ["python", "./lint.py"]
---
+++ tests.py
@@ -0,0 +1,24 @@
+import io
+import sys
+import unittest
+
+from lint import lint
+
+class Tests(unittest.TestCase):
+  def setUp(self):
+    self.stdout_backup = sys.stdout
+    self.out = io.StringIO()
+    sys.stdout = self.out
+
+  def tearDown(self):
+    sys.stdout = self.stdout_backup
+
+  def stdout(self):
+    return self.out.getvalue().strip()
+
+  def test_missing_timeout(self):
+    lint('test-data/missing-job-timeout.yml')
+    self.assertEqual(self.stdout(), '::error file=test-data/missing-job-timeout.yml,line=7,col=4::Please explicitly set timeout-minutes (to a low value) to prevent run-away jobs')
+
+if __name__ == '__main__':
+  unittest.main()
